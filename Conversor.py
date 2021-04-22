b807201ee0fffff3ea9c3352108a9098df5a7fe9
Juan Ardila <juanardila1210@gmail.com>
2021-04-22 01:54:01 -0500

    Conversor de monedas en python

---
+++ Conversor.py
@@ -0,0 +1,21 @@
+menu= """
+Bienvenido al conversor de monedas 💫
+
+1-Pesos Colombianos
+2-Pesos Argentinos 
+3-Pesos Mexicanos
+
+Elige una opción:
+"""
+opcion=int(input(menu))
+monto=float
+if opcion ==1:
+    dinero1= round(opcion/3800,2)
+    print("Tu dinero en dolares es",dinero1,"US")
+elif opcion ==2:
+     dinero2= round(opcion/3800,2)
+     print("Tu dinero en dolares es",dinero2,"US")
+elif opcion ==3:
+     dinero3== round(opcion/3800,2)
+     print("Tu dinero en dolares es",dinero3,"US")
+   
