6558053c20b843a06c11b1a38f026c8ca37f662b
Juan Ardila <juanardila1210@gmail.com>
2021-04-22 02:02:04 -0500

    Conversor optimizado con python, estructura inicial de cabecera

--- Conversor.py
+++ Conversor.py
@@ -10,14 +10,13 @@ Elige una opcion:
 def operacion(x):
     dinero=round(monto/x,2)
     print("Tu dinero en dolares es",dinero,"US")   
-opcion=int(input(menu))
-if opcion != 1 and opcion !=2 and opcion !=3:
-    print("Escribe un valor correcto")     
+opcion=int(input(menu))   
+monto=float(input("Escribe tu cantidad de dinero:"))
+if opcion ==1:
+    operacion(3800)
+elif opcion ==2:
+    operacion(65)
+elif opcion ==3:
+    operacion(24)
 else:
-    monto=float(input("Escribe tu cantidad de dinero:"))
-    if opcion ==1:
-        operacion(3800)
-    elif opcion ==2:
-        operacion(65)
-    elif opcion ==3:
-        operacion(24) 
+    print("Ingrese una opcion de moneda correcta")    
