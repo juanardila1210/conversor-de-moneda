9674d19e2e6db01910bc61bd42755505eb87598a
Juan Ardila <juanardila1210@gmail.com>
2021-04-22 01:58:34 -0500

    Conversor de monedas en python mixto 2

--- Conversor.py
+++ Conversor.py
@@ -1,21 +1,23 @@
 menu= """
-Bienvenido al conversor de monedas 💫
+Bienvenido al conversor de monedas
 
 1-Pesos Colombianos
 2-Pesos Argentinos 
 3-Pesos Mexicanos
 
-Elige una opción:
+Elige una opcion:
 """
+def operacion(x):
+    dinero=round(monto/x,2)
+    print("Tu dinero en dolares es",dinero,"US")   
 opcion=int(input(menu))
-monto=float
-if opcion ==1:
-    dinero1= round(opcion/3800,2)
-    print("Tu dinero en dolares es",dinero1,"US")
-elif opcion ==2:
-     dinero2= round(opcion/3800,2)
-     print("Tu dinero en dolares es",dinero2,"US")
-elif opcion ==3:
-     dinero3== round(opcion/3800,2)
-     print("Tu dinero en dolares es",dinero3,"US")
-   
+if opcion != 1 and opcion !=2 and opcion !=3:
+    print("Escribe un valor correcto")     
+else:
+    monto=float(input("Escribe tu cantidad de dinero:"))
+    if opcion ==1:
+        operacion(3800)
+    elif opcion ==2:
+        operacion(65)
+    elif opcion ==3:
+        operacion(24) 
